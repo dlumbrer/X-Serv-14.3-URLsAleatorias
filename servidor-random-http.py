7c3a6d1a2fad21d7bacd6ffacd81bca76b415ff5
David Moreno Lumbreras <dmorenolumb@gmail.com>
2015-02-10 19:03:11 +0100

    Realizado ejercicio

---
+++ servidor-random-http.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import socket
+import random
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+mySocket.bind(('localhost', 1234))
+
+mySocket.listen(5)
+
+
+try:
+    while True:
+        print 'Waiting for connections'
+        (recvSocket, address) = mySocket.accept()
+        print 'Request received:'
+        print recvSocket.recv(2048)
+        print 'Answering back...'
+        recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
+                        "<html><body>" +
+                        "<p>Hola, <a href='http://localhost:1234/" +
+                        str(random.randrange(10000000000000)) +
+                        "'>Dame Otra!!</a></p>" +
+                        "</body></html>" +
+                        "\r\n")
+        recvSocket.close()
+except KeyboardInterrupt:
+    print "Closing binded socket"
+    mySocket.close()
